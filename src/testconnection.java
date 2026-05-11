1b333703fd290779d0f6040e4ae1a2d079bd8353
SAFARI OUSMAN <safariousman02@gmail.com>
2026-05-11 03:11:09 +0100

    displaying the tables

--- src/testconnection.java
+++ src/testconnection.java
@@ -1,21 +1,17 @@
-
 import java.sql.*;
 
 public class testconnection {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-
+    public static void main(String[] args) throws Exception {  // ← ADD "throws Exception"
         String url = "jdbc:postgresql://localhost:5432/student_db";
         String user = "postgres";
         String password = "15H16h17";
-
-        try {
-            Connection conn = DriverManager.getConnection(url, user, password);
-            System.out.println("connected to database");
-            conn.close();
-        }catch (SQLException e) {
-                System.out.println("failed to connect to database");
-            }
-
+        
+        Connection con = DriverManager.getConnection(url, user, password);
+        ResultSet rs = con.createStatement().executeQuery("SELECT * FROM students");
+        
+        while (rs.next()) {
+            System.out.println(rs.getString("name") + " - " + rs.getInt("grade"));
+        }
+        con.close();
     }
 }
